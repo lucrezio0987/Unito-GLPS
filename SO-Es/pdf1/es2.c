2c5af24ccd41656bc8a7dbee85c39a0bc787f553
simo1701 <simobergesio@gmail.com>
2022-11-07 14:50:51 +0100

    ES 2 PDF 1

--- SO-Es/pdf1/es2.c
+++ SO-Es/pdf1/es2.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char* parse_nome(char* stringa_completa) {
+    char *nome = malloc(sizeof(char) * 30);
+    int i = 0;
+    while(isalpha(stringa_completa[i])) {
+        nome[i] = stringa_completa[i];
+        i++;
+    }
+    return nome;
+}
 
 int main() {
-    
+    char stringa[30];
+    char *nome;
+
+    fgets(stringa, 30, stdin); //oppure usando gets(stringa)
+    while(stringa[0] != '\n') {
+        nome = parse_nome(stringa);
+        printf("%s\n", nome);
+        fgets(stringa, 30, stdin); //gets(stringa)
+    } 
 }
