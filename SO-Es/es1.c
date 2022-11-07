922cad859cc45ad717c3ee47723c5211d50824cc
simo1701 <simobergesio@gmail.com>
2022-11-07 13:37:33 +0100

    creaione Es2.c di SO-Es

--- SO-Es/es1.c
+++ SO-Es/es1.c
@@ -2,43 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-char **leggi_lista(FILE *file, int *nof_elements) {
-  char **lista=malloc(sizeof(char*)*(*nof_elements));
-  char temp[30];
-  int temp_n;
-  for(int i=0; i<(*nof_elements); ++i){
-    fgets(temp, 30, file);
-    temp_n=strlen(temp);
-    temp[temp_n-1]='\0';
-    lista[i]=malloc(sizeof(char)*(temp_n));
-    strcpy(lista[i],temp);
-  }
-  return lista;
+char ** leggi_lista(FILE* file_in, int* nof_elements) {
+    char **lista = malloc(sizeof(char*)*(*nof_elements));
+    char temp[30];
+
+    for (int i = 0; i < (*nof_elements); ++i) {
+        fgets(temp, 30, file_in);
+        if (temp[0] != '\r') {
+            lista[i] =  malloc(sizeof(char)*(strlen(temp)));
+            strcpy(lista[i], temp);
+        } else --i;
+    }
+    return lista;
 }
-void stampa_lista(char **mio_ar, int n_elems){
-  for(int i=0; i<n_elems; ++i)
-    printf("%s\n", mio_ar[i]);
-  return;
+
+void stampa_lista(char** mio_ar, int n_elems) {
+    for (int i = 0; i < n_elems; i++)
+        printf("%s", mio_ar[i]);
+    return;
 }
+
 int conta(FILE *file) {
-  int i=0;
-  char temp[30];
-  while(fgets(temp, 30, file)!=NULL) ++i;
-  return i;
+    int i = 0;
+    char temp[30];
+    while (fgets(temp, 30, file) != NULL) {
+        if (temp[0] != '\r') 
+            i++;
+    }
+    return i;
 }
 
-int main(){
-  FILE *fl;
-  int n_elementi;
-  char **lista;
+int main() {
+    FILE *f1;
+    int n_elementi;
+    char **lista;
 
-  fl=fopen("file.txt", "r");
-  n_elementi=conta(fl);
-  fclose(fl);
+    f1 = fopen("file.txt", "r");
+    n_elementi = conta(f1);
 
-  lista=leggi_lista(fopen("file","r"), &n_elementi);
-
-  stampa_lista(lista, n_elementi);
-
-  exit(0);
+    lista = leggi_lista(fopen("file.txt", "r"), &n_elementi);
+    stampa_lista(lista, n_elementi);
+    exit(0);
 }
