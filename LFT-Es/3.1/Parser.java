f919fccf4a511a66c84707cc752c6988f683d1bc
Giovanni <grillogiovanni123@gmail.com>
2022-11-09 10:57:12 +0100

    reimpostazione codice base "Parser.java" in "LFT-Es/3.1"

--- LFT-Es/3.1/Parser.java
+++ LFT-Es/3.1/Parser.java
@@ -1,110 +1,74 @@
 import java.io.*;
 
 public class Parser {
-    private Lexer lex;
-    private BufferedReader pbr;
-    private Token look;
+	private Lexer lex;
+	private BufferedReader pbr;
+	private Token look;
 
-    public Parser(Lexer l, BufferedReader br) {
-        lex = l;
-        pbr = br;
-        move();
-    }
-
-    void move() {
-        look = lex.lexical_scan(pbr);
-        System.out.println("token = " + look);
-    }
-
-    void error(String s) {
-	throw new Error("near line " + lex.line + ": " + s);
-    }
-
-    void match(int t) {
-	if (look.tag == t) {
-	    if (look.tag != Tag.EOF) move();
-	} else error("syntax error");
-    }
-
-    public void start() {
-	// ... completare ...
-	expr();
-	match(Tag.EOF);
-	// ... completare ...
-    }
-
-    private void expr() {
-	// ... completare ...
-	term();
-	exprp();
-    }
-
-    private void exprp() {
-	switch (look.tag) {
-	case '+':
+	public Parser(Lexer l, BufferedReader br) {
+		lex = l;
+		pbr = br;
 		move();
-		term();
-		exprp();
-		break;
-	case '-':
-		move();
-		term();
-		exprp();
-		break;
-	default:
-		if(look.tag==Tag.ID) error("procedura exprp");
-		move();
-		break;
-	// ... completare ...
 	}
-    }
 
-    private void term() {
-	fact();
-	termp();
-        // ... completare ...
-    }
-
-    private void termp() {
-	switch(look.tag){
-	case '*':
-		move();
-		fact();
-		termp();
-		break;
-	case '/':
-		move();
-		fact();
-		termp();
-		break;
-	default:
-		if(look.tag==Tag.ID) error("procedura termp");
-		move();
-		break;
+	void move() {
+		look = lex.lexical_scan(pbr);
+		System.out.println("token = " + look);
 	}
-        // ... completare ...
-    }
 
-    private void fact() {
-	if(look.tag=='(') {
-		move();
+	void error(String s) {
+		throw new Error("near line " + lex.line + ": " + s);
+	}
+
+	void match(int t) {
+		if (look.tag == t) {
+			if (look.tag != Tag.EOF)
+				move();
+		} else
+			error("syntax error");
+	}11
+
+	public void start() {
+		// ... completare ...
 		expr();
-		match(')');
-	} else {
-		match(256);
+		match(Tag.EOF);
+		// ... completare ...
 	}
-        // ... completare ...
-    }
 
-    public static void main(String[] args) {
-        Lexer lex = new Lexer();
-        String path = "prova.txt"; // il percorso del file da leggere
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(path));
-            Parser parser = new Parser(lex, br);
-            parser.start();
-            System.out.println("Input OK");
-            br.close();
-        } catch (IOException e) {e.printStackTrace();}
-    }
+	private void expr() {
+		// ... completare ...
+	}
+
+private void exprp() {
+switch (look.tag) {
+case ’+’:
+// ... completare ...
+}
+}
+
+	private void term() {
+		// ... completare ...
+	}
+
+	private void termp() {
+		// ... completare ...
+	}
+
+	private void fact() {
+		// ... completare ...
+	}
+
+	public static void main(String[] args) {
+		Lexer lex = new Lexer();
+		String path = "...path..."; // il percorso del file da leggere
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			Parser parser = new Parser(lex, br);
+			parser.start();
+			System.out.println("Input OK");
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
