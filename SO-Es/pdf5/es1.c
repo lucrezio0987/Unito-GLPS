41748ad426e206193aa0ebba490ecd8e9bcad71e
simo1701 <simobergesio@gmail.com>
2022-11-23 14:57:59 +0100

    ES 1 PDF 5

--- SO-Es/pdf5/es1.c
+++ SO-Es/pdf5/es1.c
@@ -4,6 +4,11 @@
 #include <time.h>
 #include <errno.h>
 
+#define ERROR if(errno) { \
+    printf("%d: pid %ld (%s)\n", 1, (long)getpid(), errno, strerror(errno)); \
+    exit(1); \
+}
+
 struct mymsg {
     long mtype;
     char mtext[30];
@@ -12,16 +17,12 @@ struct mymsg {
 int main() {
     int id;
 
-    if ((id = msgget(IPC_PRIVATE, IPC_CREAT | 0600)) == -1) {
-        printf("msgget error \n");
-        exit(1);
-    }
+    if ((id = msgget(IPC_PRIVATE, IPC_CREAT | 0600)) == -1) 
+        ERROR;
 
     struct msqid_ds myqueue;
-    if(msgctl(id, IPC_STAT, &myqueue) == -1) {
-        printf("msgctl error \n");
-        exit(1);
-    }
+    if(msgctl(id, IPC_STAT, &myqueue) == -1) 
+        ERROR;
 
     printf("Dimensione della coda: %d \n", myqueue.msg_qbytes);
     printf("Numero di messaggi in coda: %d \n", myqueue.msg_qnum);
@@ -29,23 +30,18 @@ int main() {
 
     struct mymsg message = {sizeof(char), "hello world"};
 
-    if ((msgsnd(id, &message, (sizeof(message)-sizeof(long)), IPC_NOWAIT)) == -1) {
-        printf("msgsnd error \n");
-        exit(1);
-    }
+    if ((msgsnd(id, &message, (sizeof(message)-sizeof(long)), IPC_NOWAIT)) == -1) 
+        ERROR;
 
-    if(msgctl(id, IPC_STAT, &myqueue) == -1) {
-        printf("msgctl error \n");
-        exit(1);
-    }
+    if(msgctl(id, IPC_STAT, &myqueue) == -1) 
+        ERROR;
 
     printf("Numero di messaggi in coda: %d \n", myqueue.msg_qnum);
     printf("Tempo dell'ultima msg snd(): %d \n", myqueue.msg_stime);
     printf("Tempo in cui è occorsa la msgsnd: %d \n", myqueue.msg_ctime);
 
     if(msgctl(id, IPC_RMID, &myqueue) == -1) {
-        printf("mstctl error");
-        exit(1);
+        ERROR;
     } else 
         printf("Coda deallocata \n");
 
