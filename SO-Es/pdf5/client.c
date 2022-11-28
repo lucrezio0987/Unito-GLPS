032ca055026cff7fa1ba11323e3c78cff2b0ab2b
Lucrezio0987 <lucrezio0987@gmail.com>
2022-11-28 10:25:41 +0100

    server-client (ora funziona davvero)

--- SO-Es/pdf5/client.c
+++ SO-Es/pdf5/client.c
@@ -4,8 +4,9 @@
 #include <sys/msg.h>
 
 #define CTOS 1
+#define S_TERM 2
 #define ERROR if(errno) {                                                                               \     
-    printf("ERROR - l %d: pid %ld - n %d - (%d)\n", __LINE__, (long)getpid(), errno, strerror(errno));  \
+    printf("ERROR - l %d: pid %ld - n %d - (%s)\n", __LINE__, (long)getpid(), errno, strerror(errno));  \
     exit(1);                                                                                            \
 }
 
@@ -18,27 +19,32 @@ struct msg {
 };
 
 int main() {
-
     int id;
     struct msqid_ds myqueue;
     struct msg messageC;
     struct msg messageS;
     char buf[BUF_SIZE];
 
-    if ((id = msgget(ftok("vuoto", 'a'), IPC_CREAT | 0600)) < 0)
-        ERROR;
+    //--CONNEZZIONE ALLA CODA DI MESSAGGI--
+    if ((id = msgget(ftok("vuoto", 'a'), IPC_CREAT | 0600)) < 0) ERROR;
     
+    //--MESSAGGIO AL SERVER--
     printf("Client: Inserisci testo da stamoare in rosso: ");
     fgets(buf, BUF_SIZE, stdin);
-    messageC.clientPID = (long)getpid();
-    messageC.mtype = CTOS;
-    sprintf(messageC.mtext, "%s", buf);
-                    
-    if(msgsnd(id, &messageC, (sizeof(messageC) - sizeof(long)), IPC_NOWAIT) == -1)
-        ERROR;
-    
-    if(msgrcv(id, &messageS, (sizeof(messageS) - sizeof(long)), (long)getpid(), MSG_NOERROR) == -1)
-        ERROR;
+        messageC.mtype = CTOS;                  // mtype  = INVIO
+        sprintf(messageC.mtext, "%s", buf);     // mtext
+        messageC.clientPID = (long)getpid();    // clientPID
+    if(msgsnd(id, &messageC, (sizeof(messageC) - sizeof(long)), IPC_NOWAIT) == -1) ERROR;
+
+    //--ATTESA e STAMPA RITORNO--
+    if(msgrcv(id, &messageS, (sizeof(messageS) - sizeof(long)), (long)getpid(), MSG_NOERROR) == -1) ERROR;
     printf("Client: %s", messageS.mtext);
+
+    //--TERMINZAIONE SERVER--
+        messageC.mtype = S_TERM;                // mtype = TERMINZAIONE
+        sprintf(messageC.mtext, "%s", buf);     // mtext
+        messageC.clientPID = (long)getpid();    // clientPID
+    if(msgsnd(id, &messageC, (sizeof(messageC) - sizeof(long)), IPC_NOWAIT) == -1) ERROR;
+    
     exit(0);
 }
--- SO-Es/pdf5/server.c
+++ SO-Es/pdf5/server.c
@@ -3,8 +3,10 @@
 #include <errno.h>
 #include <sys/msg.h>
 
+#define CTOS 1
+#define S_TERM 2
 #define ERROR if(errno) {                                                                               \     
-    printf("ERROR - l %d: pid %ld - n %d - (%d)\n", __LINE__, (long)getpid(), errno, strerror(errno));  \
+    printf("ERROR - l %d: pid %ld - n %d - (%s)\n", __LINE__, (long)getpid(), errno, strerror(errno));  \
     exit(1);                                                                                            \
 }
 
@@ -20,52 +22,47 @@ struct msg {
     long clientPID;
 };
 
-int main() {
+int main() { 
 
     int id;
     struct msqid_ds myqueue;
     struct msg messageC;
     struct msg messageS; 
     
-    if ((id = msgget(ftok("vuoto", 'a'), IPC_CREAT | 0600)) < 0)
-        ERROR;
+    //--CREAZIONE DELLA CODA DI MESSAGGI--
+    if ((id = msgget(ftok("vuoto", 'a'), IPC_CREAT | 0600)) < 0) ERROR;
     
-   // for(;;)
-     {
-        msgctl(id, IPC_STAT, "myqueue");
+    //--CICLO DI ASCOLTO--
+    for(;;) {
+        msgctl(id, IPC_STAT, &myqueue);
+        if(myqueue.msg_qnum != 0) {
+            //--ATTESA MESSAGGIO--
+            if(msgrcv(id, &messageC, (sizeof(messageC) - sizeof(long)), CTOS, IPC_NOWAIT) == -1) ERROR;
 
-
-        if(myqueue.msg_qnum != 0) {   
-            
-            if(msgrcv(id, &messageC, (sizeof(messageC) - sizeof(long)), 1, MSG_NOERROR) == -1)
-                ERROR;
-
+            //--GESIONE MESSAGGIO NOMALE--
             switch (fork()) {
-                case -1:
-                    ERROR;
-                
+                case -1: ERROR;
                 case 0:
+                //--ELABORAZIONE MESASGGIO--
                     printf("--> Server: %sMessaggio ricevuto dal server%s\n", BLU, WHITE);
                     messageS.mtype = messageC.clientPID;
                     messageS.clientPID=messageC.clientPID;
                     sprintf(messageS.mtext, "%s%s%s", RED, messageC.mtext, WHITE);
-                    
-                    if(msgsnd(id, &messageS, (sizeof(messageS) - sizeof(long)), IPC_NOWAIT) == -1)
-                        ERROR;
+                //--INVIO RIPOSTA--
+                    if(msgsnd(id, &messageS, (sizeof(messageS) - sizeof(long)), IPC_NOWAIT) == -1) ERROR;
                     printf("<-- Server: %sMessaggio inviato dal server%s\n", BLU, WHITE);
                     exit(0);
-
-                default:
-                    wait();
-                    break;
+                default: wait(); break;
             }
-        
-        if(msgctl(id, IPC_RMID, &myqueue) == -1) {
-            ERROR;
-        } else 
-            printf("Coda deallocata \n");
-        exit(0);
-            
+
+            //--GESTIONE MESSAGGIO DI USCITA--
+            if(msgrcv(id, &messageC, (sizeof(messageC) - sizeof(long)), S_TERM, IPC_NOWAIT) == -1) ERROR;
+            if(messageC.mtype==S_TERM) break;
         }
     }
+
+    //-- DEALLOCAZIONE CODA + TERMINAZIONE SERVER--
+    if(msgctl(id, IPC_RMID, &myqueue) == -1) { ERROR; } 
+    else  printf("Server: Coda deallocata \n");
+    exit(0);
 }
