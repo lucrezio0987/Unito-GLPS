52ab57788b5993ca560467a48edf2e0d6fcb0f0c
Lucrezio0987 <lucrezio0987@gmail.com>
2022-11-29 11:34:36 +0100

    librria semafori

---
+++ SO-Es/pdf7/lib_sem.h
@@ -0,0 +1,52 @@
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <errno.h>
+
+#define ERROR if(errno) {                                                                               \     
+    printf("ERROR - l %d: pid %ld - n %d - (%s)\n", __LINE__, (long)getpid(), errno, strerror(errno));  \
+    exit(1);                                                                                            \
+}
+
+typedef struct my_data {
+    char buf[30];
+} data;
+
+union semun {
+    int val;                
+    struct semid_ds *buf;   
+    ushort *array;          
+    struct seminfo *__buf;  
+    void *__pad;
+};
+
+int reserveSem(int id_sem, int n_sem) {
+    struct sembuf s_ops;
+
+    s_ops.sem_num = n_sem;
+    s_ops.sem_op = -1;
+    s_ops.sem_flg = 0;
+
+    return semop(id_sem, &s_ops, 1);
+}
+
+int releaseSem(int id_sem, int n_sem) {
+    struct sembuf s_ops;
+
+    s_ops.sem_num = n_sem;
+    s_ops.sem_op = 1;
+    s_ops.sem_flg = 0;
+
+    return semop(id_sem, &s_ops, 1);
+}
+
+int initSemAvailable(int semID, int semNum) {
+    union semun arg;
+    arg.val = 1;
+    return semctl(semID, semNum, SETVAL, arg);
+}
+
+int initSemInUse(int semId, int semNum) {
+    union semun arg;
+    arg.val = 0;
+    return semctl(semId, semNum, SETVAL, arg);
+}
