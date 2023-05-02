3b8e1f1533044e05a45d8850dc26d9ada7b9c00d
Lucrezio Del Ponte <lucrezio.delponte@edu.unito.it>
2023-05-02 18:32:49 +0200

    commit

--- ASD/Laboratorio/Es1/src/Interfaccia.h
+++ ASD/Laboratorio/Es1/src/Interfaccia.h
@@ -1,33 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #ifndef ORDERED_ARRAY_H_laokjsdnbudjllvfidkfmqm
 #define ORDERED_ARRAY_H_laokjsdnbudjllvfidkfmqm
 
 typedef struct _Array Array;
-typedef struct _Record Record;
+typedef struct _Record Records;
 
 // Funzioni di comparazione
-int ComparePos(Record* i, Record* j);
-int CompareInt(Record* i, Record* j);
-int CompareFloat(Record* i, Record* j);
-int CompareString(Record* i, Record* j);
-
-// Funzioni di supporto
-void printRecord(Array* A, int i_el, void *fp);
+int ComparePos(Records* i, Records* j);
+int CompareInt(Records* i, Records* j);
+int CompareFloat(Records* i, Records* j);
+int CompareString(Records* i, Records* j);
 
 // Funzioni di Creazione e riempomento Array
-Array* ArrayCreate(unsigned short field);
-void arrayAdd(Array* A, Record *rec);
+Records** CreateArray();
+void arrayAdd(Array* A, Records *rec);
+void LoadArray(Array *A, const char *infile);
+void PrintArray(const char *outfile, Array *A);
 
 // Algoritmo Merge-Sort
-void Merge(Array* A, unsigned int l , unsigned int m, unsigned int r, unsigned int nitems);
-void MergeSort(Array* A, unsigned int i, unsigned int j, unsigned int nitems);
+void Merge(void **base, unsigned int l , unsigned int m, unsigned int r, int (*compar)(const void *, const void*));
+void MergeSort(void **base, unsigned int i, unsigned int j, int (*compar)(const void *, const void*));
 
 // Algoritmo di Bynary-Insertion-Sort
-void Insert(Array* A, int i, int loc);
-int search(int x, Array* A, int i, int j);
-void BineryInsertionSort(Array* A, unsigned int nitems);
+void Insert(void** base, unsigned int i, unsigned int loc, int (*compar)(const void *, const void*));
+int search(unsigned int x, void** base, unsigned int i, unsigned int j, int (*compar)(const void *, const void*));
+void BinaryInsertionSort(void** base,  unsigned int nitems, int (*compar)(const void *, const void*));
 
 // Macro-Funzioni
-void merge_binary_insertion_sort(void *A, size_t nitems, unsigned short int field, size_t k);
-int sort_records(const char *infile, const char *outfile, size_t k, size_t field);
+void merge_binary_insertion_sort(void **base, size_t nitems, size_t k, int (*compar)(const void *, const void*));
+void sort_records(const char *infile, const char *outfile, size_t k, size_t field);
 
 #endif /* ORDERED_ARRAY_H_laokjsdnbudjllvfidkfmqm */
--- ASD/Laboratorio/Es1/src/Ordinamento.c
+++ ASD/Laboratorio/Es1/src/Ordinamento.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 #include "Interfaccia.h"
 
 #define INPUT_FILE "../records.csv"
 #define OUTPUT_FILE "bin/outfile.csv"
 
+#define K 100
+
+enum enum_field {FIELD_POS, FIELD_STRING, FIELD_INT, FIELD_FLOAT};
+
 void main(int argc, const char* argv[]) {
-    int field, n_records;
-
-    if(argc != 3) {
-        printf("use default field(0) e n_records(10)\n");
-        field = 0;
-        n_records = 10;
-    } else {
-        n_records = atoi(argv[1]);
-        field = atoi(argv[2]);
-    }
-
-    sort_records(INPUT_FILE, OUTPUT_FILE, field, n_records);
+        
+    if(argc == 3)
+        sort_records(argv[1], argv[2], K, FIELD_POS);
+    else 
+        sort_records(INPUT_FILE, OUTPUT_FILE, K, FIELD_INT);
 }
+
+// Si prevede che la funzione venga chiamata con (altrimenti verranno usati i valori di default): 
+//    ./bin/Ordinamento [INPUT_FILE] [OUTPUT_FILE]
