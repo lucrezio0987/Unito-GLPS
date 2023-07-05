c1410d5d95e27d9160109be606cb7737a12790c3
Lucrezio Del Ponte <lucrezio.delponte@edu.unito.it>
2023-07-05 12:17:11 +0200

    commit

--- ASD/Laboratorio/esegui.sh
+++ ASD/Laboratorio/esegui.sh
@@ -1,44 +1,37 @@
 #!/bin/bash
 
-Menu_2() {
-    case $1 in
-        1) 
-            cd ex1
-            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mTEST da implementare"
-            cd ..
-            ;;
-        2) 
-            cd ex2
-            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mTEST da implementare"
-            cd ..
-            ;;
-        3) 
-            cd ex3
-            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mTEST da implementare"
-            cd ..
-            ;;
-        4) 
-            cd ex4
-            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mTEST da implementare"
-            cd ..
-            ;;
-        5) 
-            ;;
-        *) 
-            tput cnorm
-            tput rmcup -T xterm
-            tput sgr0
-            clear
-            exit 0
-            ;;
-    esac    
-}
+# Ottiene le dimensioni del terminale
+rows=$(tput lines)
+cols=$(tput cols)
+tput civis      # Nasconde il cursore del terminale
+tput rmcup      # Disabilita lo scrolling del terminale
 
 Menu_1() {
+    clear
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e "\033[1;32m:: PROGETTO DI ALGORITMI ::"
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e ""
+    echo -e "\033[1;34m  -.\033[1;37m Eserczio 1: ex1 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m     1.\033[0;37m Esegui\033[0;33m[\033[0;35mX\033[0;33m]"
+    echo -e "\033[1;34m     2.\033[0;37m Unit test\033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 2: ex2 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 3: ex3 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 4: ex4 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  *.\033[0;32m << Torna Indietro"
+    echo -e "\n"
+    echo -e "\033[0;91m NOTE:"
+    echo -e "\033[0;36m"
+    cat note.txt
+    echo -e "\n"
+    echo -e ""
+    echo -e -n "\033[1;32mScegli un'opzione: \033[1;34m"
+    read scelta
+    echo -e "\033[1;37m"
 
-    case $1 in
-        1)
-            cd ex1
+    clear
+    case $scelta in
+        1)  # ESEGUI
             echo -e "\033[1;32m::\033[0;91m MAKE:"
             echo -e "\033[0;33m"
             make
@@ -51,15 +44,48 @@ Menu_1() {
             echo -e "\033[0;37m"
             cat outfile.csv
             echo -e ""
-            echo -e "\033[1;32m::\033[0;91m NOTE:"
-            echo -e "\033[0;36m"
-            cat note.txt
-            echo -e ""
             echo -e "\033[1;32m::\033[0;91m USCITA..."
-            cd ..
             ;;
-        2)
-            cd ex2
+        2)  # TEST
+            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mTEST da implementare"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    echo -e "\n\n"
+    echo -e "\033[0;32m << Torna al Menu..."
+    echo -e "\033[1;37m"
+    read -n1 -s -r
+}
+
+Menu_2() {
+    clear
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e "\033[1;32m:: PROGETTO DI ALGORITMI ::"
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e ""
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 1: ex1 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m  -.\033[1;37m Eserczio 2: ex2 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m     1.\033[0;37m Esegui\033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m     2.\033[0;37m Unit test\033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 3: ex3 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 4: ex4 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  *.\033[0;32m << Torna Indietro"
+    echo -e "\n"
+    echo -e "\033[0;91m NOTE:"
+    echo -e "\033[0;36m"
+    cat note.txt
+    echo -e "\n"
+    echo -e ""
+    echo -e -n "\033[1;32mScegli un'opzione: \033[1;34m"
+    read scelta
+    echo -e "\033[1;37m"
+
+    clear
+    case $scelta in
+        1)  # ESEGUI
             echo -e "\033[1;32m::\033[0;91m MAKE:"
             echo -e "\033[0;33m"
             make
@@ -68,63 +94,150 @@ Menu_1() {
             echo -e "\033[0;36m"
             ./build/main_ex2
             echo -e ""
-            echo -e "\033[1;32m::\033[0;91m NOTE:"
-            echo -e "\033[0;36m"
-            cat note.txt
-            echo -e ""
             echo -e "\033[1;32m::\033[0;91m USCITA..."
-            cd ..
             ;;
-        3)
+        2)  # TEST
+            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mTEST da implementare"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    echo -e "\n\n"
+    echo -e "\033[0;32m << Torna al Menu..."
+    echo -e "\033[1;37m"
+    read -n1 -s -r
+}
+
+Menu_3() {
+    clear
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e "\033[1;32m:: PROGETTO DI ALGORITMI ::"
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e ""
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 1: ex1 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 2: ex2 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m  -.\033[1;37m Eserczio 3: ex3 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 4: ex4 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  *.\033[0;32m << Torna Indietro"
+    echo -e "\n"
+    echo -e "\033[0;91m NOTE:"
+    echo -e "\033[0;36m"
+    cat note.txt
+    echo -e "\n"
+    echo -e ""
+    echo -e -n "\033[1;32mScegli un'opzione: \033[1;34m"
+    read scelta
+#    echo -e "\033[1;37m"
+#   
+#    clear
+#    case $scelta in
+#        1)  # ESEGUI
+#            echo -e "\033[1;32m::\033[0;91m MAKE:"
+#            echo -e "\033[0;33m"
+#            make
+#            echo -e ""
+#            echo -e "\033[1;32m::\033[0;91m ESECUZIONE:"
+#            echo -e "\033[0;36m"
+#            ./build/main_ex2
+#            echo -e ""
+#            echo -e "\033[1;32m::\033[0;91m USCITA..."
+#            ;;
+#        2)  # TEST
+#            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mTEST da implementare"
+#            ;;
+#        *)
+#            return 1
+#            ;;
+#    esac
+#
+#    echo -e "\n\n"
+#    echo -e "\033[0;32m << Torna al Menu..."
+#    echo -e "\033[1;37m"
+#    read -n1 -s -r
+}
+
+Menu_4() {
+    clear
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e "\033[1;32m:: PROGETTO DI ALGORITMI ::"
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e ""
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 1: ex1 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 2: ex2 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m  -.\033[0;37m Eserczio 3: ex3 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  -.\033[1;37m Eserczio 4: ex4 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  *.\033[0;32m << Torna Indietro"
+    echo -e "\n"
+    echo -e "\033[0;91m NOTE:"
+    echo -e "\033[0;36m"
+    cat note.txt
+    echo -e "\n"
+    echo -e ""
+    echo -e -n "\033[1;32mScegli un'opzione: \033[1;34m"
+    read scelta
+#    echo -e "\033[1;37m"
+#
+#    clear
+#    case $scelta in
+#        1)  # ESEGUI
+#            echo -e "\033[1;32m::\033[0;91m MAKE:"
+#            echo -e "\033[0;33m"
+#            make
+#            echo -e ""
+#            echo -e "\033[1;32m::\033[0;91m ESECUZIONE:"
+#            echo -e "\033[0;36m"
+#            ./build/main_ex2
+#            echo -e ""
+#            echo -e "\033[1;32m::\033[0;91m USCITA..."
+#            ;;
+#        2)  # TEST
+#            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mTEST da implementare"
+#            ;;
+#        *)
+#            return 1
+#            ;;
+#    esac
+#
+#    echo -e "\n\n"
+#    echo -e "\033[0;32m << Torna al Menu..."
+#    echo -e "\033[1;37m"
+#    read -n1 -s -r
+}
+
+Menu_0() {
+    clear
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e "\033[1;32m:: PROGETTO DI ALGORITMI ::"
+    echo -e "\033[1;32m:::::::::::::::::::::::::::"
+    echo -e ""
+    echo -e "\033[1;34m  1.\033[0;37m Eserczio 1: ex1 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m  2.\033[0;37m Eserczio 2: ex2 \033[0;33m[\033[0;35m-\033[0;33m]"
+    echo -e "\033[1;34m  3.\033[0;37m Eserczio 3: ex3 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  4.\033[0;37m Eserczio 4: ex4 \033[0;33m[\033[0;35m \033[0;33m]"
+    echo -e "\033[1;34m  *.\033[0;32m Esci dallo script"
+    echo -e ""
+    echo -e -n "\033[1;32mScegli un'opzione: \033[1;34m"
+    read scelta
+    echo -e "\033[1;37m"
+    
+    case $scelta in
+        1)  
+            cd ex1
+            Menu_1
+            ;;
+        2)  
+            cd ex2
+            Menu_2
+            ;;
+        3)  
             cd ex3
-            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mINTERO ESERCIZZIO da implementare"
-            echo -e ""
-            echo -e "\033[1;32m::\033[0;91m NOTE:"
-            echo -e "\033[0;36m"
-            cat note.txt
-            echo -e ""
-            cd ..
+            Menu_3
             ;;
         4)  
-            cd ex3
-            echo -e "\033[1;32m::\033[0;91m TODO: \033[0;37mINTERO ESERCIZZIO da implementare (non so nemmeno dove sia la consegna)"
-            echo -e ""
-            echo -e "\033[1;32m::\033[0;91m NOTE:"
-            echo -e "\033[0;36m"
-            cat note.txt
-            echo -e ""
-            cd ..
-            ;;
-        5) 
-            scelta=1
-            while [ $scelta -ne 5 ]; do
-                clear
-                echo -e "\033[1;32m:::::::::::::::::::::::::::"
-                echo -e "\033[1;32m:: PROGETTO DI ALGORITMI ::"
-                echo -e "\033[1;32m:::::::::::::::::::::::::::"
-                echo -e ""
-                echo -e "\033[0;34m  -.\033[0;37m Compila ed esegui \033[1;37mex1 \033[0;33m[\033[0;35mX\033[0;33m]"
-                echo -e "\033[0;34m  -.\033[0;37m Compila ed esegui \033[1;37mex2 \033[0;33m[\033[0;35mX\033[0;33m]"
-                echo -e "\033[0;34m  -.\033[0;37m Compila ed esegui \033[1;37mex3 \033[0;33m[\033[0;35m \033[0;33m]"
-                echo -e "\033[0;34m  -.\033[0;37m Compila ed esegui \033[1;37mex4 \033[0;33m[\033[0;35m \033[0;33m]"
-                echo -e "\033[0;34m  -.\033[0;32m Test"
-                echo -e "\033[1;34m     1.\033[0;37m Test \033[1;37mex1 \033[0;33m[\033[0;35m \033[0;33m]"
-                echo -e "\033[1;34m     2.\033[0;37m Test \033[1;37mex2 \033[0;33m[\033[0;35m \033[0;33m]"
-                echo -e "\033[1;34m     3.\033[0;37m Test \033[1;37mex3 \033[0;33m[\033[0;35m \033[0;33m]"
-                echo -e "\033[1;34m     4.\033[0;37m Test \033[1;37mex4 \033[0;33m[\033[0;35m \033[0;33m]"
-                echo -e "\033[1;34m     5.\033[0;32m Torna indietro"
-                echo -e "\033[1;34m  *.\033[0;32m Esci dallo script"
-                echo -e ""
-                echo -e -n "\033[1;32mScegli un'opzione: \033[1;34m"
-                read scelta
-                echo -e ""
-
-                Menu_2 "$scelta"
-                if [ "$scelta" -ne 5 ]; then
-                    echo -e "\033[1;37m"
-                    read -n1 -s -r
-                fi
-            done    
+            cd ex4
+            Menu_4
             ;;
         *)
             tput cnorm
@@ -134,44 +247,11 @@ Menu_1() {
             exit 0
             ;;
     esac
+
+    cd ..
 }
-
-# Ottiene le dimensioni del terminale
-rows=$(tput lines)
-cols=$(tput cols)
-
-# Nasconde il cursore del terminale
-tput civis
-
-# Disabilita lo scrolling del terminale
-tput rmcup
-
-
-# Imposta il testo del terminale su bianco
-tput setaf 7
 
 while true; do
     clear
-
-    echo -e "\033[1;32m:::::::::::::::::::::::::::"
-    echo -e "\033[1;32m:: PROGETTO DI ALGORITMI ::"
-    echo -e "\033[1;32m:::::::::::::::::::::::::::"
-    echo -e ""
-    echo -e "\033[1;34m  1.\033[0;37m Compila ed esegui \033[1;37mex1 \033[0;33m[\033[0;35mX\033[0;33m]"
-    echo -e "\033[1;34m  2.\033[0;37m Compila ed esegui \033[1;37mex2 \033[0;33m[\033[0;35mX\033[0;33m]"
-    echo -e "\033[1;34m  3.\033[0;37m Compila ed esegui \033[1;37mex3 \033[0;33m[\033[0;35m \033[0;33m]"
-    echo -e "\033[1;34m  4.\033[0;37m Compila ed esegui \033[1;37mex4 \033[0;33m[\033[0;35m \033[0;33m]"
-    echo -e "\033[1;34m  5.\033[0;32m Test"
-    echo -e "\033[1;34m  *.\033[0;32m Esci dallo script"
-    echo -e ""
-    echo -e -n "\033[1;32mScegli un'opzione: \033[1;34m"
-    read scelta
-    echo -e "\033[1;37m"
-
-    Menu_1 "$scelta"
-
-    if [ "$scelta" -ne 5 ]; then
-        echo -e "\033[1;37m"
-        read -n1 -s -r
-    fi
+    Menu_0
 done
