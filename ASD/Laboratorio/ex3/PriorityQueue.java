8a079e030e55961b82ad2dd6d0b87741e3607d8a
simo1701 <simobergesio@gmail.com>
2023-08-05 09:53:50 +0200

    commit

--- ASD/Laboratorio/ex3/PriorityQueue.java
+++ ASD/Laboratorio/ex3/PriorityQueue.java
@@ -18,16 +18,75 @@ public class PriorityQueue<E> implements AbstractQueue<E> {
     @Override
     public boolean push(E e) {
       heap.add(e);
-      int i = heap.size() - 1;
-      int p = i - 1;
-      while (i > 0 && comparator.compare(heap.get(i), heap.get(p)) < 0) {
-        swap(i, p);
-        i = p;
-        p = p - 1;
-      }
+      heapifyUp(heap.size() - 1);
       return true;
+      /*
+      int i = (heap.size()) / 2;
+      heap.add(i, e);
+      
+      minHeap(i);
+      return true;*/
     }
-    
+    /*
+    private void minHeap(int i) {
+      while (i > 0 && i <= heap.size() - 2) {
+        if (comparator.compare(heap.get(i), heap.get(i - 1)) < 0) {
+          swap(i, i - 1);
+          i = i - 1;
+        } else if (comparator.compare(heap.get(i), heap.get(i + 1)) > 0) {
+          swap(i, i + 1);
+          i = i + 1;
+        } else break;
+      }
+    }*/
+
+    private int parentIndex(int index) {
+      return (index - 1) / 2;
+    }
+  
+    private int leftChildIndex(int index) {
+        return 2 * index + 1;
+    }
+  
+    private int rightChildIndex(int index) {
+        return 2 * index + 2;
+    }
+
+    private void fixHeap(int index) {
+      E e = heap.get(index);
+      if (index == 0 || comparator.compare(e, heap.get(parentIndex(index))) >= 0) 
+        heapifyDown(index);
+      else 
+        heapifyUp(index);
+    }
+
+    private void heapifyUp(int index) {
+      while (index > 0) {
+        if (comparator.compare(heap.get(parentIndex(index)), heap.get(index)) <= 0) break;
+        swap(parentIndex(index), index);
+        index = parentIndex(index);
+      }
+    }
+
+    private void heapifyDown(int index) {
+      int left = leftChildIndex(index);
+      int right = rightChildIndex(index);
+      while (left < heap.size()) {
+        int smallestChildIndex = left;
+
+        if (right < heap.size() && comparator.compare(heap.get(right), heap.get(left)) < 0)
+          smallestChildIndex = right;
+        
+        if (comparator.compare(heap.get(index), heap.get(smallestChildIndex)) <= 0)
+          break;
+
+        swap(index, smallestChildIndex);
+        index = smallestChildIndex;
+        left = leftChildIndex(index);
+        right = rightChildIndex(index);
+      }
+    }
+
     private void swap(int i, int j) {
       E temp = heap.get(i);
       heap.set(i, heap.get(j));
@@ -44,6 +103,7 @@ public class PriorityQueue<E> implements AbstractQueue<E> {
     public void pop() {
         if (this.heap.isEmpty()) return;
         (this.heap).remove(0);
+        heapifyDown(0);
     }
 
     @Override
@@ -82,6 +142,10 @@ public class PriorityQueue<E> implements AbstractQueue<E> {
         priorityQueue.push(1);
         priorityQueue.push(3);
 
+        System.out.println(priorityQueue); // Stampa: [1, 2, 10, 5]
+        priorityQueue.pop();
+        System.out.println(priorityQueue); // Stampa: [1, 2, 10, 5]
+        priorityQueue.pop();
         System.out.println(priorityQueue); // Stampa: [1, 2, 10, 5]
     }
 };
