82c59de6fe0418d4411b9198a0538e5f7b9bb02c
Lucrezio Del Ponte <lucrezio.delponte@edu.unito.it>
2023-08-09 13:07:41 +0200

    daasd

--- ASD/Laboratorio/ex3/ex3.java
+++ ASD/Laboratorio/ex3/ex3.java
@@ -4,7 +4,7 @@ public class ex3 {
     public static void main(String[] args) {
         Random random = new Random();
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Integer::compare);
-        
+        int flag = 0; 
         int i, tot = 10 ;
 
         for(i = 0; i< tot; ++i)
@@ -13,17 +13,21 @@ public class ex3 {
         System.out.println("Fine Inserimento\n");
 
         System.out.println("Coda: "  + priorityQueue + " \n ");
+        
         System.out.println("contains 3: " + priorityQueue.contains(3));
         System.out.println("contains 17: " + priorityQueue.contains(17) + " \n ");
+
         System.out.println("remove 3: " + priorityQueue.remove(3));
         System.out.println("remove 17: " + priorityQueue.remove(17) + " \n ");
         
         while(!priorityQueue.empty()){
           System.out.println("Top: " + priorityQueue.top() + "(" + priorityQueue.sortered_Array.get(0).getE() + ")   Coda: "  + priorityQueue);
+          if(priorityQueue.top() != priorityQueue.sortered_Array.get(0).getE()) flag++;
+          
           System.out.println("> Pop Eseguita...");
           priorityQueue.pop();
         }
 
-        System.out.println("\nFine Rimozione");
+        System.out.println("\nFine Rimozione:   elementi non minimi = " + flag);
     }
 };
