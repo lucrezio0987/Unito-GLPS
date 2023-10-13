59ce8533798834c25e9ffa3733717a02f63f2824
Lucrezio Del Ponte <lucrezio.delponte@edu.unito.it>
2023-10-13 16:30:33 +0200

    ASD: piccola fixatina al comparatore degli archi

--- II Anno/ASD/Laboratorio/ex4/src/ArchComparator.java
+++ II Anno/ASD/Laboratorio/ex4/src/ArchComparator.java
@@ -3,10 +3,6 @@ import java.util.Comparator;
 public class ArchComparator<E> implements Comparator<Arch<E>> {
     @Override
     public int compare(Arch<E> arch1, Arch<E> arch2) {
-        if (arch1.getDistance() < arch2.getDistance())
-            return -1;
-        else if (arch1.getDistance() > arch2.getDistance())
-            return 1;
-        return 0;
+		return Float.compare(arch1.getDistance(), arch2.getDistance());
     }
 }
