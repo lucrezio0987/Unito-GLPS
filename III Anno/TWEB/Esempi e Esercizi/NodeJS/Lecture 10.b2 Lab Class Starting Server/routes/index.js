5867e0e05f2f4b165da0eb35f0679773927a7f7f
Lucrezio Del Ponte <lucrezio.delponte@edu.unito.it>
2023-10-17 13:27:40 +0200

    TWEB: aggiunta soluzioni

--- III Anno/TWEB/Esempi e Esercizi/NodeJS/Lecture 10.b2 Lab Class Starting Server/routes/index.js
+++ III Anno/TWEB/Esempi e Esercizi/NodeJS/Lecture 10.b2 Lab Class Starting Server/routes/index.js
@@ -10,7 +10,25 @@ router.route('/')
 
 router.route('/add')
     .post(function (req, res) {
-        res.render('index', {title: 'Please implement the supporting server adding '+req.body.no1+'+'+req.body.no2})
+
+        let no1 = req.body.no1;
+        let no2 = req.body.no2;
+
+        let headers = {
+            method: 'post',
+            body: JSON.stringify(no1, no2),
+            headers: {'Content-Type': 'application/json'},
+            user-agent: 'localhost:3000'
+        }
+        fetch('http://localhost:3001/do_something', headers)
+        .then(res => res.json()) // expecting a json response e.g. {field1: 'xxx', field 2: 'yyy'}
+        .then(json =>
+            res.render('index', {title: " results is: “+json.field2}))
+        .catch(err =>
+            res.render('index', {title: err}))
+
+
+        //res.render('index', {title: 'Please implement the supporting server adding '+req.body.no1+'+'+req.body.no2})
     });
 
 module.exports = router;
