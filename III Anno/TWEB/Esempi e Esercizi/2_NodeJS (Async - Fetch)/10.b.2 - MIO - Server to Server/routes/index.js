1fcc2bd3f59b80c40fed9a6495a739d4a73d7940
Lucrezio Del Ponte <lucrezio.delponte@edu.unito.it>
2023-10-23 15:30:06 +0200

    TWEB: Es terver to server funzioannte

--- III Anno/TWEB/Esempi e Esercizi/2_NodeJS (Async - Fetch)/10.b.2 - MIO - Server to Server/routes/index.js
+++ III Anno/TWEB/Esempi e Esercizi/2_NodeJS (Async - Fetch)/10.b.2 - MIO - Server to Server/routes/index.js
@@ -6,23 +6,22 @@ const fetch = require('node-fetch');
 router.route('/')
     .get(function (req, res){
         res.render('index', {title: 'My Class'});
-    });
-
-router.route('/add')
+    })
     .post(function (req, res) {
 //        res.render('index', {title: 'Please implement the supporting server adding '+req.body.no1+'+'+req.body.no2})
+        let no1 = req.body.no1;
+        let no2 = req.body.no2;
 
         let Headers = {
             method: 'post',
-            body: JSON.stringify( {no1:req.body.no1, no2:req.body.no2} ),
-            roheaders: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( {no1:no1, no2:no2} ),
+            headers: { 'Content-Type': 'application/json' },
         }
 
-        fetch('http://localhost:3000/add', Headers)
+        fetch('http://localhost:3002/add', Headers)
             .then(res => res.json())
             .then(json => res.render('index', {title: " results is: " + json.result}))
             .catch(err => res.render('index', {title: " ERRORE: " + err}));
-
 
     });
 
