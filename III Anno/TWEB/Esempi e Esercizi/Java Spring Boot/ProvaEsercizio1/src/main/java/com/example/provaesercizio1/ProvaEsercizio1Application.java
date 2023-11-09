5df182ea66953b7c66bad93210fe52cd13b0909e
Lucrezio Del Ponte <lucrezio.delponte@edu.unito.it>
2023-11-09 12:14:07 +0100

    TWEB: first commit prova springboot

---
+++ III Anno/TWEB/Esempi e Esercizi/Java Spring Boot/ProvaEsercizio1/src/main/java/com/example/provaesercizio1/ProvaEsercizio1Application.java
@@ -0,0 +1,13 @@
+package com.example.provaesercizio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaEsercizio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaEsercizio1Application.class, args);
+	}
+
+}
---
+++ III Anno/TWEB/Esempi e Esercizi/Java Spring Boot/ProvaEsercizio1/src/test/java/com/example/provaesercizio1/ProvaEsercizio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provaesercizio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaEsercizio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
