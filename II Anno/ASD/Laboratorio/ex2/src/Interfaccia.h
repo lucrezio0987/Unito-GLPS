5ce669f6142fac4f43c1953f4b34ad2899899565
Lucrezio Del Ponte <lucrezio.delponte@edu.unito.it>
2023-12-01 11:30:50 +0100

    commit

--- II Anno/ASD/Laboratorio/ex2/src/Interfaccia.h
+++ II Anno/ASD/Laboratorio/ex2/src/Interfaccia.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * @file Interfaccia.h
  * @brief Declarations and definitions for managing a dictionary implemented with a SkipList.
