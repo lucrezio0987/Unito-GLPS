7456a8b94144b99001fcb88ffed2c6da0417507b
simo1701 <simobergesio@gmail.com>
2024-01-15 18:25:14 +0100

    commenti userdata

--- III Anno/PROG/Progetto/Prova Server/src/main/java/com/example/prova_server/model/UserData.java
+++ III Anno/PROG/Progetto/Prova Server/src/main/java/com/example/prova_server/model/UserData.java
@@ -15,17 +15,18 @@ import java.util.stream.Stream;
 
 public class UserData {
     private String username;
-    private Map<String, Mail> mailSent;
-    private Map<String, Mail> mailReceived;
+    private Map<String, Mail> mailSent; // mappa mail inviate <uuid, mail>
+    private Map<String, Mail> mailReceived; // mappa mail ricevute <uuid, mail>
     private boolean connected;
     private String address = null;
     private int broadcastPort = 0;
     private int mailPort = 0;
 
     private final String[] MAIL_HEADER = {"Uuid", "Sender", "Recipients", "Object", "Text", "CreationDateTime", "LastModifyDateTime", "read"};
-    private String csvMailSendPath = null;
-    private String csvMailReceivedPath = null;
+    private String csvMailSendPath = null; // percorso file mail inviate
+    private String csvMailReceivedPath = null; // percorso file mail ricevute
 
+    // costruttori
     public UserData() {
         this.username = null;
         this.mailSent = new HashMap<>();
@@ -56,6 +57,8 @@ public class UserData {
     public String getUsername() {
         return this.username;
     }
+
+    // restituisce l'ultima modifica effettuata a una mail, 01/01/0001 se non ci sono mail e di conseguenza modifiche
     public String getLastModifyData() {
         return Stream.concat(mailSent.values().stream(), mailReceived.values().stream())
                 .filter(Objects::nonNull)
@@ -64,39 +67,54 @@ public class UserData {
                 .findFirst()
                 .orElse("01/01/0001 00:00:00");
     }
+
+    // restituisce la mappa delle mail inviate con il parametro delete = false
     public synchronized Map<String, Mail> getMailSentNotDelete() {
         return mailSent.entrySet()
                 .stream()
                 .filter(entry -> !entry.getValue().isDelete())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
+
+    // restituisce la mappa delle mail ricevute con il parametro delete = false
     public synchronized Map<String, Mail> getMailReceivedNotDelete() {
         return mailReceived.entrySet()
                 .stream()
                 .filter(entry -> !entry.getValue().isDelete())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
+
+    // restituisce la mappa di mail inviate
     public synchronized Map<String, Mail> getMailSent() {
         return mailSent;
     }
+
+    // restituisce la mappa di mail ricevute
     public synchronized Map<String, Mail> getMailReceived() {
         return mailReceived;
     }
+
+    // restituisce le mail inviate (dopo la data passatagli come parametro) (usata lato server)
     public synchronized Map<String, Mail> getMailSent(String lastConnectionDatatime) {
         return  mailSent.entrySet().stream()
                 .filter(m -> m.getValue().moreRecentlyOf(lastConnectionDatatime))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
+
+    // restituisce le mail ricevute (dopo la data passatagli come parametro) (usata lato server)
     public synchronized Map<String, Mail> getMailReceived(String lastConnectionDatatime) {
         return  mailReceived.entrySet().stream()
                 .filter(m -> m.getValue().moreRecentlyOf(lastConnectionDatatime))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
+
+    // restituisce le mail ricevute più recenti della data passata come parametro
     public synchronized Map<String, Mail> getMailSentListMoreRecentlyOf(String date) {
         return  mailSent.values().stream()
                 .filter(m -> m.moreRecentlyOf(date))
                 .collect(Collectors.toMap(Mail::getUuid, mail -> mail));
     }
+    // restituisce le mail ricevute più recenti della data passata come parametro
     public synchronized Map<String, Mail> getMailReceivedListMoreRecentlyOf(String date) {
         return  mailReceived.values().stream()
                 .filter(m -> m.moreRecentlyOf(date))
@@ -141,6 +159,7 @@ public class UserData {
         backup();
     }
 
+    // chiamato quando lo username del client viene aggiornato
     public void updateUsername(String newUsername) {
         if (this.username != null) {
             if(!username.equals(newUsername))
@@ -148,48 +167,58 @@ public class UserData {
         } else
             setUsername(newUsername);
     }
+    // aggiorna tutte le mail inviate, con le modifiche che ci sono state dall'ultima connessione (alla riconnessione)
     public synchronized void updateMailSent(Map<String, Mail> updates) {
         mailSent.putAll(updates);
         backup();
     }
+    // aggiorna tutte le mail ricevute, con le modifiche che ci sono state dall'ultima connessione (alla riconnessione)
     public synchronized void updateMailReceived(Map<String, Mail> updates) {
         mailReceived.putAll(updates);
         backup();
     }
+    // aggiunge la mail inviata alla mappa mailSent
     public synchronized void addMailSent(Mail mail) {
         this.mailSent.put(mail.getUuid(), mail);
         backup();
     }
+    // aggiunge le mail ricevute alla mappa mailReceived
     public synchronized void addMailReceived(Mail mail) {
         this.mailReceived.put(mail.getUuid(), mail);
         backup();
     }
-
+    // cancelli la lista locale, senza cancellare le mail, esempio quando si cambia utente
     public void clearMailListReceived() { mailReceived.clear(); }
+    // cancelli la lista locale, senza cancellare le mail, esempio quando si cambia utente
     public void clearMailListSent() {
         mailSent.clear();
     }
+    // cancella la lista di mail ricevute (delete all)
     public synchronized void deleteMailListReceived() {
         mailReceived.values().forEach(Mail::setDelete);
         backup();
     }
+    // cancella la lista di mail inviate (delete all)
     public synchronized void deleteMailListSent() { mailSent.values().forEach(Mail::setDelete); backup(); }
+    // cancella la mail inviata con parametro uuid
     public synchronized void deleteMailSent(String uuid) {
         getMailSentNotDelete().get(uuid).setDelete();
         backup();
     }
+    // cancella la mail ricevuta con parametro uuid
     public synchronized void deleteMailReceived(String uuid) {
         getMailReceivedNotDelete().get(uuid).setDelete();
         backup();
     }
 
-    // BACKUP
+    // load BACKUP aggiorna la mail sent e la mail received leggendo il file
     private synchronized void loadBackup() {
         if(this.username != null) {
             mailSent = readCSVMailSender();
             mailReceived = readCSVMailReceiver();
         }
     }
+    // backup aggiorna i file dopo ad esempio l'invio o la cancellazione di una mail
     private synchronized void backup() {
         if(this.username != null) {
             writeCSVMailSender();
@@ -197,7 +226,7 @@ public class UserData {
         }
     }
 
-    // CSV
+    // CSV scrittura
     private void writeCSVMail(String path, Map<String, Mail> mailList){
         Map<String, Mail> mailMap = readCSVMail(path);
         mailList.values().forEach(mail -> mailMap.put(mail.getUuid(), mail));
@@ -224,6 +253,8 @@ public class UserData {
     private void writeCSVMailReceiver(){
         writeCSVMail(csvMailReceivedPath, mailReceived);
     }
+
+    // CSV lettura
     private Map<String, Mail> readCSVMail(String path) {
         createFileIfNotExists(path);
 
@@ -275,6 +306,8 @@ public class UserData {
             throw new RuntimeException(e);
         }
     }
+
+    // genera il percorso del file
     public String pathConstructor(String username, String type) {
         String directoryPath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "backup";
         File directory = new File(directoryPath);
