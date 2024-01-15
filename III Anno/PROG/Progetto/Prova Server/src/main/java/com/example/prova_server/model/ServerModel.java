470824d938abe63acc51af1c658e3934bb9c78b3
simo1701 <simobergesio@gmail.com>
2024-01-15 23:28:05 +0100

    commenti

--- III Anno/PROG/Progetto/Prova Server/src/main/java/com/example/prova_server/model/ServerModel.java
+++ III Anno/PROG/Progetto/Prova Server/src/main/java/com/example/prova_server/model/ServerModel.java
@@ -251,7 +251,7 @@ public class ServerModel {
         return userDataList.get(username).getAddress();
     }
 
-    // scambi di connessione
+    // handler delle connessioni e disconnessioni
     private static class ConnectionHandler implements Runnable  {
         private Socket socket;
 
@@ -288,7 +288,7 @@ public class ServerModel {
 
                     connectionInfo = new ConnectionInfo(ports.get(0), ports.get(1), userDataList.get(username).getLastModifyData());
 
-                    // INVIO: data ultima modifica del server
+                    // INVIO: data ultima modifica del server più le porte su cui inviare mail e messaggi
                     System.out.println(":::::::   INIZIO -> INVIO: data ultima modifica del server + porte == lastModifyData: " + userDataList.get(username).getLastModifyData());
                     outputStream.writeObject(new Gson().toJson(connectionInfo));
                     outputStream.flush();
@@ -300,9 +300,12 @@ public class ServerModel {
                     Map<String, Map<String, Mail>> mapMailClient = new Gson().fromJson((String) inputStream.readObject(), type);
                     System.out.println(":::::::   FINE -> RICEZIONE: modifiche del client offline == mapMailClient: " + mapMailClient.get("sent").size() + " " + mapMailClient.get("received").size());
 
+                    // creazione mappe mail ricevute e inviate quando il client era offline
                     Map<String, Mail> mapMailSentClient = mapMailClient.get("sent");
                     Map<String, Mail> mapMailReceivedClient = mapMailClient.get("received");
                     sendNotSent(mapMailSentClient);
+
+                    // creazione mappe mail ricevute e inviate dall'ultima modifica del server
                     Map<String, Mail> mapMailSentServer = userDataList.get(username).getMailSent(LastConnectionDatatime);
                     Map<String, Mail> mapMailReceivedServer = userDataList.get(username).getMailReceived(LastConnectionDatatime);
 
@@ -314,6 +317,7 @@ public class ServerModel {
                     userDataList.get(username).updateMailSent(combinedSentMap);
                     userDataList.get(username).updateMailReceived(combinedReceivedMap);
 
+                    // crea le liste combinate
                     Map<String, Map<String, Mail>> mapMailServer = new HashMap<>();
                     mapMailServer.put("sent", combinedSentMap);
                     mapMailServer.put("received", combinedReceivedMap);
@@ -335,13 +339,14 @@ public class ServerModel {
 
                 outputStream.close();
                 inputStream.close();
-
+                // chiusura socket
                 socket.close();
             } catch (JsonSyntaxException | IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
     }
+    // handler per la ricezione di mail dai client, invoca il sendMail per inviare la mail
     private static class MessageHandler implements Runnable {
         private Socket socket;
 
@@ -352,6 +357,7 @@ public class ServerModel {
         @Override
         public void run() {
             try {
+                // ricezione di una mail da parte di un client
                 ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
                 String jsonMail = (String) inputStream.readObject();
                 Mail mail = new Gson().fromJson(jsonMail, Mail.class);
@@ -364,6 +370,7 @@ public class ServerModel {
 
                 mail.getRecipientsList().forEach(recipient -> sendMail(recipient, mail));
 
+                // chiusura socket
                 socket.close();
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
@@ -380,12 +387,14 @@ public class ServerModel {
         @Override
         public void run() {
             try {
+                // ricezione modifiche da parte di un client
                 ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
                 String jsonModifyInfo = (String) inputStream.readObject();
                 MailModifyInfo modifyInfo = new Gson().fromJson(jsonModifyInfo, MailModifyInfo.class);
                 Mail mail = modifyInfo.getMail();
                 String username = modifyInfo.getUsername();
 
+                // aggiorna la cancellazione di una o tutte le mail dalla lista
                 if(modifyInfo.getSent()) {
                     if (modifyInfo.isDeleteAll())
                         userDataList.get(username).deleteMailListSent();
@@ -407,18 +416,20 @@ public class ServerModel {
                 if (modifyInfo.isRead())
                     log("MODIFY: ("+username+") read");
 
+                // chiusura socket
                 socket.close();
             } catch (JsonSyntaxException | IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
     }
-
+    // invia le mail non ancora inviate, alla riconnessione
     private static void sendNotSent(Map<String, Mail> mapMailSentClient) {
         mapMailSentClient.values().stream()
                 .filter(mail -> !mail.isDelete() && !mail.getRead())
                 .forEach(mail -> mail.getRecipientsList().forEach(recipient -> sendMail(recipient, mail)));
     }
+    // invia la mail al destinatario
     private static void sendMail(String recipient, Mail mail) {
         boolean success;
         int i;
@@ -432,7 +443,7 @@ public class ServerModel {
         if (userDataList.get(recipient).isOn()) {
             for (i = 0, success = false; i < 5 && !success; ++i)
                 try {
-
+                    // apre un socket sulla porta di invio delle mail
                     Socket socket = new Socket(userDataList.get(recipient).getAddress(), userDataList.get(recipient).getMailPort());
 
                     ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
@@ -449,6 +460,7 @@ public class ServerModel {
 
                     inputStream.close();
                     outputStream.close();
+                    // chiusura socket
                     socket.close();
                 } catch (IOException | ClassNotFoundException e) {
                     throw new RuntimeException(e);
@@ -461,6 +473,7 @@ public class ServerModel {
         } else
             log("Inoltro a: " + recipient + " - CLIENT OFFLINE (Mail Salvata)");
     }
+    // invio messaggio di stop
     private void clientBroadcastStopMessage(String address, int broadcastPort) {
         try (Socket socket = new Socket(address, broadcastPort)) {
             try (ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())) {
@@ -474,7 +487,9 @@ public class ServerModel {
             System.out.println("Invio fallito del messaggio di disattivazione del server");
         }
     }
-
+    // restituisce una mappa combinata della client e server
+    // se entrambe sono delete = true o delete = false, restituisce quella con l'ultima modifica più recente
+    // in caso contrario, restituisce quella con la delete = true
     public static Map<String, Mail> combineMailMaps(Map<String, Mail> mapClient, Map<String, Mail> mapServer) {
         log("  [+] MAPPA 1 Client: " + mapClient.values().size() + " ( di cui " + mapClient.values().stream().filter(Mail::isDelete).count() + " cancellate)");
         log("  [+] MAPPA 2 Server: " + mapServer.values().size() + " ( di cui " + mapServer.values().stream().filter(Mail::isDelete).count() + " cancellate)");
@@ -496,6 +511,8 @@ public class ServerModel {
         log("  [+] MAPPA 1+2 Comb: " + combMap.values().size() + " ( di cui " + combMap.values().stream().filter(m -> m.isDelete()).count() + " cancellate)");
         return combMap;
     }
+    // metodo di scelta delle porte per lo scambio di mail e di messaggi
+    // sceglie la prima libera dalla 8010 alla 9000, sull'indirizzo passato come parametro
     private synchronized static List<Integer> selectPort(String address) {
 
         List<Integer> occupiedPorts = userDataList.values().stream()
@@ -503,13 +520,6 @@ public class ServerModel {
                 .filter(d -> d.getAddress().equals(address))
                 .flatMap(d -> Stream.of(d.getBroadcastPort(), d.getMailPort()))
                 .toList();
-
-//        for(UserData d : userDataList.values())
-//            if(d.getClientAddress().equals(address) && d.isOn()){
-//                occupiedPorts.add(d.getBroadcastPort());
-//                occupiedPorts.add(d.getMailPort());
-//            }
-
 
         List<Integer> ports = new ArrayList<>();
 
@@ -520,6 +530,7 @@ public class ServerModel {
         return ports;
     }
 
+    // crea newLine che vengono stampate nella text dei log
     private static synchronized void log(String newLine) {
         if (newLine.isEmpty())
             newLine = "ALLERT: String is NULL";
@@ -535,6 +546,7 @@ public class ServerModel {
         logList.add(newLine);
     }
 
+    // effettua la lettura dei backup di log
     private void loadBackupLog() {
         if(FileExist(pathConstructor(null, "log")))
             try (Reader reader = new FileReader(pathConstructor(null, "log"));
@@ -551,6 +563,7 @@ public class ServerModel {
                 e.printStackTrace();
             }
     }
+    // scrive il log.csv
     private void backupLog() {
         logList.add("------------------------------------------[ " +
                 new SimpleDateFormat("dd/MM/yy HH:mm:ss").format(new Date())
@@ -564,12 +577,13 @@ public class ServerModel {
             e.printStackTrace();
         }
     }
-
+    // svuota i file di backup
     public void clearAllBackup() {
         clearBackupLog();
         clearBackupMail();
         log("BACKUP: Rimossi tutti i file di backup");
     }
+    // svuota i backup delle mail
     public void clearBackupMail() {
         Arrays.stream(Objects.requireNonNull(new File(
                         System.getProperty("user.dir") +
@@ -583,6 +597,7 @@ public class ServerModel {
 
         log("BACKUP: Rimossi i file csv di backup delle mail (Inviate e Ricevute)");
     }
+    // svuota i backup dei log
     public void clearBackupLog() {
 
         Arrays.stream(Objects.requireNonNull(new File(
@@ -595,7 +610,7 @@ public class ServerModel {
         textAreaProperty.set(null);
         log("BACKUP: pulita la storiografia dei log (log.csv)");
     }
-
+    // aggiorna l'elenco di utenti che si sono collegati al server
     private Set<String> getSetUsernamesSaved() {
         Set<String> utentiConDati = new HashSet<>();
 
@@ -617,6 +632,7 @@ public class ServerModel {
     private static boolean FileExist(String path) {
         return Files.exists(Path.of(path));
     }
+    // crea il percorso dei file
     private static String pathConstructor(String username, String type){
         String directoryPath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "backup";
         File directory = new File(directoryPath);
