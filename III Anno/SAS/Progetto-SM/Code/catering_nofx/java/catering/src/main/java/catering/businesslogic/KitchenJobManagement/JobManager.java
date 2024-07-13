44296c4915a0827e14b795273c8134f9b0054905
simo1701 <simobergesio@gmail.com>
2024-07-13 20:24:00 +0200

    codice

---
+++ III Anno/SAS/Progetto-SM/Code/catering_nofx/java/catering/src/main/java/catering/businesslogic/KitchenJobManagement/JobManager.java
@@ -0,0 +1,39 @@
+package catering.businesslogic.KitchenJobManagement;
+
+import catering.businesslogic.shiftManagement.Cook;
+import catering.businesslogic.shiftManagement.Shift;
+import catering.businesslogic.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobController {
+
+    // Event sender methods
+    public void addReceiver(SummarySheetEventReceiver er) {
+
+    }
+
+    public void removeReceiver(SummarySheetEventReceiver er) {
+
+
+    }
+
+    private void notifyJobAssigned(Job job, Shift shift) {
+
+    }
+
+    // Operations methods
+    public void createJob(String title, int portions, boolean prepare, boolean completed) {
+
+    }
+
+    public void assignJob(Job job, Shift shift, Cook cook, String portions, int time) {
+
+    }
+
+    public boolean isChef(User user) {
+
+
+    }
+}
